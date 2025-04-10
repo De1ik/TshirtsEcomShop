5b24d7823a0b4b056971801eba59ad40b556de83
De1ik <olegovich.1903@gmail.com>
2025-04-10 21:08:20 +0200

    admin add product page basic

--- back-laravel/routes/web.php
+++ back-laravel/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\PageCatalogueController;
+use App\Http\Controllers\AdminCreateProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('', [MainController::class, 'index'])->name('home');
 Route::get('/catalogue', [PageCatalogueController::class, 'default'])->name('default_catalogue');
 Route::get('/login', [AuthController::class, 'login'])->name('login');
+
+Route::get('/admin/create-product', [AdminCreateProductController::class, 'create_product'])->name('create_product');
+Route::post('/admin/create-product', [AdminCreateProductController::class, 'save_new_product'])->name('save_new_product');
